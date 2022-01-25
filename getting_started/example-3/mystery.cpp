1d5b9fa144b04e5c5a4a59d57614acc4d19c0feb
Yuval <yuvalgal51@gmail.com>
2022-01-26 01:44:55 +0200

    document example-3

--- getting_started/example-3/mystery.cpp
+++ getting_started/example-3/mystery.cpp
@@ -1,14 +1,25 @@
+// mystery.cpp: ?
+// Description: Print a 9x9 multiplication table.
+
 #include <iostream>
 using namespace std;
 
-int main() {
-  cout << " 1\t2\t3\t4\t5\t6\t7\t8\t9" << endl << "" << endl;
-  for (int c = 1; c < 10; c++) {
-    cout << c << "| ";
-    for (int i = 1; i < 10; i++) {
-      cout << i * c << '\t';
+int main()
+{
+    // print horizontal header
+    cout << " 1\t2\t3\t4\t5\t6\t7\t8\t9" << endl
+         << "" << endl;
+    for (int c = 1; c < 10; c++)
+    {
+        // print vertical header
+        cout << c << "| ";
+        for (int i = 1; i < 10; i++)
+        {
+            // print multiplication
+            cout << i * c << '\t';
+        }
+        // end line after vertical header + muliplications on line
+        cout << endl;
     }
-    cout << endl;
-  }
-  return 0;
-} 
+    return 0;
+}
