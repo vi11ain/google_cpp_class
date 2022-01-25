46b5f498540d54e543cdb38df9f101c2d6143375
Yuval <yuvalgal51@gmail.com>
2022-01-25 03:00:16 +0200

    updated gitignore and added example1-experiment1

--- example-1/hello.cpp
+++ example-1/hello.cpp
@@ -1,10 +1,20 @@
-// hello.cpp: Maggie Johnson
-// Description: a program that prints the immortal saying "hello world"
+// hello.cpp: Yuval Gal
+// Description: a program that prints the immortal saying "hello world" 4 times in 6 lines
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main() {
-  cout << "Hello World!" << endl;
+int main()
+{
+  for (int i = 0; i < 6; ++i)
+  {
+    for (int j = 0; j < 4; ++j)
+      // set width of 17 spaces, the minimum number of characters that will be written, filled by spaces for blanks
+      cout << setw(17) << "Hello World!";
+    // write a newline as part of outer loop
+    cout << endl;
+  }
+
   return 0;
 }
