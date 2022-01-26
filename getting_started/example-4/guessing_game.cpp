71f14624ce6491945110a59d81e43df7abf04504
Yuval <yuvalgal51@gmail.com>
2022-01-26 03:05:33 +0200

    second step done

--- getting_started/example-4/guessing_game.cpp
+++ getting_started/example-4/guessing_game.cpp
@@ -9,17 +9,38 @@ using namespace std;
 
 int main()
 {
+    int guess = 0;
     // create seed based on time
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
     // create generator with seed
     default_random_engine generator(seed);
     // create random number distribution of 1 to 99
     uniform_int_distribution<int> distribution(1, 99);
-
     // generate random secret number
-    int secret_number = distribution(generator);
+    int secret = distribution(generator);
 
-    cout << secret_number << endl;
+    cout << "A secret number between 1 and 100 was chosen..." << endl;
+
+    cout << "Enter your guess: ";
+    if (!(cin >> guess))
+    {
+        cout << "Invalid input!" << endl;
+    }
+    else
+    {
+        if (guess < secret)
+        {
+            cout << "Secret is bigger than " << guess << endl;
+        }
+        else if (guess > secret)
+        {
+            cout << "Secret is smaller than " << guess << endl;
+        }
+        else
+        {
+            cout << "Congratz! You guessed correctly!" << endl;
+        }
+    }
 
     return 0;
 }
