85cfb6f2c3f3feb0e8740bcdf242e887908e6574
Yuval <yuvalgal51@gmail.com>
2022-01-27 16:42:50 +0200

    add recursive gcd

--- getting_started/example-4/TODO.md
+++ getting_started/example-4/TODO.md
@@ -5,4 +5,4 @@
 - [x] Learn the Euclidean algorithm
 - [x] Create a non-recursive function
 - [x] Create a small test for the function in `main()`
-- [ ] Create a recursive function
+- [x] Create a recursive function
--- getting_started/example-4/gcd.cpp
+++ getting_started/example-4/gcd.cpp
@@ -22,11 +22,28 @@ int non_recursive_gcd(int a, int b)
     } while (true);
 }
 
+int recursive_gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+
+    if (b == 0)
+        return a;
+
+    return recursive_gcd(b, a % b);
+}
+
 int main()
 {
-    cout << non_recursive_gcd(4, 12) << endl
-         << non_recursive_gcd(6, 40) << endl
-         << non_recursive_gcd(8, 37) << endl;
+    cout << "non recursive gcd:" << endl
+         << "4, 12: " << non_recursive_gcd(4, 12) << endl
+         << "6, 40: " << non_recursive_gcd(6, 40) << endl
+         << "8, 37: " << non_recursive_gcd(8, 37) << endl << endl;
+
+    cout << "recursive gcd:" << endl
+         << "4, 12: " << recursive_gcd(4, 12) << endl
+         << "6, 40: " << recursive_gcd(6, 40) << endl
+         << "8, 37: " << recursive_gcd(8, 37) << endl;
 
     return 0;
 }
