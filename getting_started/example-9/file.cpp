b0e9444779b29a69c86217d91cfcc996f9bb2d94
Yuval <yuvalgal51@gmail.com>
2022-01-28 05:27:28 +0200

    solution for final experiment

--- getting_started/example-9/file.cpp
+++ getting_started/example-9/file.cpp
@@ -9,36 +9,63 @@ int main()
     char first_name[30], last_name[30];
     int age;
     char file_name[20];
-    // Collect the data.
-    cout << "Enter First Name: ";
-    cin >> first_name;
-    cout << "Enter Last Name: ";
-    cin >> last_name;
-    cout << "Enter Age: ";
-    cin >> age;
-    cout << endl
-         << "Enter the name of the file: ";
+    char should_continue;
+
+    // get filename
+    cout << "Enter the name of the file: ";
     cin >> file_name;
 
     // Create an ofstream called People, open the stream for output.
     ofstream People(file_name, ios::out);
-    // Write the output to the stream.
-    People << first_name << endl
-           << last_name << endl
-           << age << endl;
+
+    do
+    {
+        // Collect the data.
+        cout << "Enter First Name: ";
+        cin >> first_name;
+        cout << "Enter Last Name: ";
+        cin >> last_name;
+        cout << "Enter Age: ";
+        cin >> age;
+
+        // Write the output to the stream.
+        People << first_name << endl
+               << last_name << endl
+               << age << endl;
+
+        // Prompt user to accept another entry round
+        cout << "Enter another entry (Y/N)? ";
+        cin >> should_continue;
+
+        // Stop getting input if user enter yes
+        if (should_continue != 'Y' && should_continue != 'y')
+            break;
+
+    } while (true);
+
     // Close stream
     People.close();
 
     // Create an ifstream called PeopleRead, open the stream for input.
     ifstream PeopleRead(file_name, ios::in);
-    // Read the input from the stream.
-    PeopleRead >> first_name >> last_name >> age;
+
+    while (true)
+    {
+        // Read the input from the stream.
+        PeopleRead >> first_name >> last_name >> age;
+
+        // Stop loop if reached end-of-file
+        if (PeopleRead.eof())
+            break;
+
+        // Print to stdout
+        cout << first_name << endl
+             << last_name << endl
+             << age << endl;
+    }
+
     // Close stream
     PeopleRead.close();
-
-    cout << first_name << endl
-         << last_name << endl
-         << age << endl;
 
     return 0;
 }
