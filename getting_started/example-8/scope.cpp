45fa07083d0c13246964a7b6837c889b33f12586
Yuval <yuvalgal51@gmail.com>
2022-01-28 04:51:35 +0200

    document scope

--- getting_started/example-8/scope.cpp
+++ getting_started/example-8/scope.cpp
@@ -7,20 +7,33 @@ using namespace std;
 int a = 18;
 int b = 6;
 
-int function1(int a, int b) {
-  return a - b;
+int function1(int a, int b)
+{
+    // local variables come before global variables
+    return a - b;
 }
 
-int function2() {
-  int c;
-  c = a + b;
-  return c;
+int function2()
+{
+    int c;
+    // global variables
+    // c = -6 + 6
+    // c = 0
+    c = a + b;
+    return c;
 }
 
-int main() {
-  int b = 12;
-  int c = 0;
-  a = function1(b, a);
-  c = function2();
-  cout << "a: " << a << " b: " << b << " c: " << c << endl;
+int main()
+{
+    int b = 12;
+    int c = 0;
+    // b is local, a is global
+    // a = function1(12, 18)
+    // a = -6
+    // NOTICE global variable a is now changed!
+    a = function1(b, a);
+    // c = 0
+    c = function2();
+    // a: -6 b: 12 c: 0
+    cout << "a: " << a << " b: " << b << " c: " << c << endl;
 }
